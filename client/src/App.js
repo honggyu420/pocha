d681bd5458b73dbc45f36b344288e96218c44d9d
Hong-Gyu Lee <honggyu420@gmail.com>
2021-11-13 17:31:39 -0500

    socket.io baseline implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import ChatRoom from './pages/ChatRoom'
+import ClearChat from './pages/ClearChat'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App" style={{justifyContent:'center', display: 'flex'}}>
+
+        <Routes>
+
+          <Route exact path="/" component={ChatRoom} />
+          <Route exact path="/clearChat" component={ClearChat} />
+
+
+        </Routes>
+        
+      </div>
+    </Router>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,22 +1,37 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io")
+var path = require("path")
+var uuid = require('uuid-random');
+const { uniqueNamesGenerator, adjectives, colors, animals, names } = require('unique-names-generator');
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 
 const app = express();
+
 app.use(index);
 
 const server = http.createServer(app);
 
-const io = socketIo(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true
-  }
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+
+// const io = socketIo(server, {
+//   cors: {
+//     origin: "client",
+//     methods: ["GET", "POST"],
+//     credentials: true
+//   }
+// });
+const io = socketIo(server);
 
 const getApiAndEmit = socket => {
   const response = new Date();
@@ -24,16 +39,60 @@ const getApiAndEmit = socket => {
   socket.emit("FromAPI", response);
 };
 
-let interval;
-io.on("connection", (socket) => {
+var chat_room_data = []
+var connected_clients = {}
+
+function sendUpdatedChatRoomData(client){
+  client.emit("RetrieveChatRoomData", chat_room_data)
+  client.broadcast.emit("RetrieveChatRoomData", chat_room_data)
+}
+
+io.on("connection", (client) => {
   console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
+
+  client.on("SendMessage", (message_data) => {
+    console.log("Message received: " + message_data);
+    chat_room_data.push(message_data)
+    sendUpdatedChatRoomData(client)
+  });
+
+  client.on("UserEnteredRoom", (user_data) => {
+    var entered_room_message = {
+      message: `${user_data.username} has entered the chat`,
+      username: "",
+      user_id: 0,
+      timsteamp: null,
+    }
+    chat_room_data.push(entered_room_message)
+    sendUpdatedChatRoomData(client)
+    connected_clients[client.id] = user_data
+
+  });
+
+  //Creating identity for new connected user
+  client.on("CreateUserData", () => {
+    let user_id = uuid();
+    let username = uniqueNamesGenerator({ dictionaries: [adjectives, names] });
+    var user_data = {user_id, username}
+
+    client.emit("SetUserData", user_data)
+  })
+  
+  client.on("disconnecting", (data) => {
     console.log("Client disconnected");
-    clearInterval(interval);
+  
+    if(connected_clients[client.id]) {
+      var left_room_message = {
+        message: `${connected_clients[client.id].username} has left the chat`,
+        username: "",
+        user_id: 0,
+        timestamp: null,
+      }
+  
+      chat_room_data.push(left_room_message)
+      sendUpdatedChatRoomData(client)
+      delete connected_clients[client.id]
+    }
   });
 });
 
